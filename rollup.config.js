29896d972dc00c7da6167d1844730ab54c476cef
PichotM3 <pichotcontact@gmail.com>
2020-07-11 12:26:23 -0400

    fix: rollup & fivem & minify only

--- rollup.config.js
+++ rollup.config.js
@@ -4,24 +4,23 @@ import {terser} from "rollup-plugin-terser";
 
 const input = resolve(__dirname, "src/NativeUi.ts");
 
-const file = (min) => {
-    const path = `dist/nativeui/nativeui${min ? ".min" : ""}.js`
+const file = () => {
+    const path = `dist/nativeui/nativeui.js`
     return {
         file: resolve(__dirname, path),
         name: "NativeUI",
         format: "es",
-        plugins: [ min ? terser() : void 0 ]
+        plugins: [ terser() ]
     }
 }
 
 export default {
     input,
     output: [
-        file(false),
-        file(true)
+        file()
     ],
     external: ["@citizenfx/client"],
     plugins: [
-        typescript({useTsconfigDeclarationDir: true, abortOnError: false})
+        typescript({ abortOnError: false})
     ]
 }; 
